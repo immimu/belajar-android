fc74fdbd59a4d816c22d3a5c1dd7a1f2f1009f7b
Miftah Mubarak <mubarakmiftah@gmail.com>
2020-11-26 05:16:09 +0700

    Implement popup menu

--- app/src/main/java/com/immimu/belajarandroid/ui/MenuActivity.kt
+++ app/src/main/java/com/immimu/belajarandroid/ui/MenuActivity.kt
@@ -3,15 +3,26 @@ package com.immimu.belajarandroid.ui
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.PopupMenu
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.immimu.belajarandroid.R
+import kotlinx.android.synthetic.main.activity_menu.*
 
-class MenuActivity : AppCompatActivity() {
+class MenuActivity : AppCompatActivity(), PopupMenu.OnMenuItemClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        moreButton.setOnClickListener { showMoreMenu() }
+    }
+
+    private fun showMoreMenu() {
+        val popupMenu = PopupMenu(this, moreButton)
+        popupMenu.inflate(R.menu.menu_more_action)
+        popupMenu.setOnMenuItemClickListener(this)
+        popupMenu.show()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -41,5 +52,23 @@ class MenuActivity : AppCompatActivity() {
 
     private fun showToast(text: String) {
         Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onMenuItemClick(item: MenuItem?): Boolean {
+        return when (item?.itemId) {
+            R.id.actionEdit -> {
+                showToast("Menu Edit di klik")
+                true
+            }
+            R.id.actionDelete -> {
+                showToast("Menu Delete di klik")
+                true
+            }
+            R.id.actionFavorite -> {
+                showToast("Menu Favorite di klik")
+                true
+            }
+            else -> false
+        }
     }
 }
